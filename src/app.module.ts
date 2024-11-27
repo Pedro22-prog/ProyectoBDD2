4f83b2f5241a28d09ef754059d92662bfde6defc
Pedro Gonzalez <pedro.jgonzalez73@gmail.com>
2024-11-27 12:34:19 -0400

    Finish Result

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,14 +2,14 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DriversModule } from './drivers/drivers.module';
-import { LapTimesModule } from './lap_times/lap_times.module';
+import { LapTimeModule } from './lap_times/lap_times.module';
 import { CircuitsModule } from './circuits/circuits.module';
 import { ConstructorResultsModule } from './constructor_results/constructor_results.module';
 import { ConstructorStandingsModule } from './constructor_standings/constructor_standings.module';
 import { DriverStandingsModule } from './driver_standings/driver_standings.module';
-import { PitStopsModule } from './pit_stops/pit_stops.module';
+import { PitStopModule } from './pit_stops/pit_stops.module';
 import { QualifyingModule } from './qualifying/qualifying.module';
-import { RacesModule } from './races/races.module';
+import { RaceModule } from './races/races.module';
 import { ResultsModule } from './results/results.module';
 import { SprintResultsModule } from './sprint_results/sprint_results.module';
 import { StatusModule } from './status/status.module';
@@ -17,20 +17,31 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
 import { ConstructorModule } from './constructor/constructor.module';
 
-
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
-    MongooseModule.forRootAsync(
-      {
-        useFactory:() => ({
-            uri: process.env.MONGODB_URI,
-            dbName: process.env.DB_NAME,
-        }),
-      }
-    ), DriversModule, LapTimesModule, CircuitsModule, ConstructorResultsModule, ConstructorStandingsModule, DriverStandingsModule, PitStopsModule, QualifyingModule, RacesModule, ResultsModule, SprintResultsModule, StatusModule, ConstructorModule],
+    MongooseModule.forRootAsync({
+      useFactory: () => ({
+        uri: process.env.MONGODB_URI,
+        dbName: process.env.DB_NAME,
+      }),
+    }),
+    DriversModule,
+    LapTimeModule,
+    CircuitsModule,
+    ConstructorResultsModule,
+    ConstructorStandingsModule,
+    DriverStandingsModule,
+    PitStopModule,
+    QualifyingModule,
+    RaceModule,
+    ResultsModule,
+    SprintResultsModule,
+    StatusModule,
+    ConstructorModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -5,24 +5,34 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors(
-    {
-      origin:'',
-      methods:'GET, HEAD, PUT PATCH, POST, DELETE',
-      allowedHeaders: 'Content-Type, Accept'
-    }
-  )
-  app.useGlobalPipes(new ValidationPipe({
-    whitelist: true,
-    forbidNonWhitelisted: true,
-    transform: true,
-  }));
+
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type, Accept',
+  });
+
+  app.setGlobalPrefix('api')
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+
   const config = new DocumentBuilder()
-  .setTitle('NestJS API')
-  .setDescription('Estadisticas de Formula')
-  .addTag('nestjs').build();
+    .setTitle('Proyecto de formula 1')
+    .setDescription('Estadísticas de Fórmula 1')
+    .setVersion('1.0')
+    .addTag('nestjs')
+    .build();
+
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
+
 bootstrap();
